f3430eec3bc84b1e78f3a25aa1c9066caa2903ed
jadonhuang-hash <jadon.huang@students.tka.org>
2025-12-05 12:02:11 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
-let servo_pos = randint(0, 10)
+function changeServoPos_left () {
+    pins.servoWritePin(AnalogPin.P0, servo_pos)
+    basic.pause(7.5)
+    servo_pos += -1
+}
+function changeServoPos_right () {
+    pins.servoWritePin(AnalogPin.P0, servo_pos)
+    basic.pause(7.5)
+    servo_pos += 1
+}
+function stopServo () {
+	
+}
+let servo_pos = 0
+servo_pos = 180
+let Servo_Range = randint(0, 180)
 basic.forever(function () {
-    while (false) {
-    	
-    }
+	
 })
